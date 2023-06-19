3c29f906531d953db0ffb2473778e583b76b4480
Crowdbotics <engineering@crowdbotics.com>
2023-06-19 23:18:17 +0000

    Initial commit.

---
+++ backend/withered_truth_dev_93104/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for withered_truth_dev_93104 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'withered_truth_dev_93104.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-truth-dev-93104",
+  url: "https://withered-truth-dev-93104.botics.co",
+  api: "https://withered-truth-dev-93104.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
